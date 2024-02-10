46517abefbc6d65e060dc52de6847260c5d13f51
Roho7 <rohosen2@gmail.com>
2024-02-10 21:06:41 +0000

    :fix [transfer puzzle zone logic inside player logic, input field appears on E key press]

--- game_files/Assets/Scenes/Shared Scripts/PlayerLogic.cs
+++ game_files/Assets/Scenes/Shared Scripts/PlayerLogic.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class PlayerLogic : MonoBehaviour
 {
@@ -8,23 +10,30 @@ public class PlayerLogic : MonoBehaviour
     public float speed = 0.01f;
     public float jumpforce = 5;
     private bool hasJumped = false;
+    public GameObject textInputFieldHolder;
+    public QuestionAreaLogic questionAreaLogic;
+    public Text questionText;
+    public Text instructionText;
 
-
+    private string instruction = "";
 
     // Start is called before the first frame update
 
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
     void OnCollisionEnter2D(Collision2D collision)
     {
 
         hasJumped = false;
 
     }
+    void Start()
+    {
+        questionAreaLogic = GameObject.FindGameObjectWithTag("QuestionArea").GetComponent<QuestionAreaLogic>();
+        questionText = GameObject.FindGameObjectWithTag("Question").GetComponent<Text>();
+        instructionText = GameObject.FindGameObjectWithTag("InGameInstruction").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+
     void Update()
     {
 
@@ -34,10 +43,6 @@ public class PlayerLogic : MonoBehaviour
                 player.velocity = Vector3.up * jumpforce;
                 hasJumped = true;
             }
-            // if (Input.GetKey(KeyCode.S))
-            // {
-            //     player.transform.position += new Vector3(0, -speed, 0);
-            // }
             if (Input.GetKey(KeyCode.A))
             {
                 player.transform.position += new Vector3(-speed, 0, 0);
@@ -47,6 +52,24 @@ public class PlayerLogic : MonoBehaviour
             {
                 player.transform.position += new Vector3(speed, 0, 0);
                 player.transform.localScale = new Vector3(-1, 1, 1);
+            }
+            if (!questionAreaLogic.isInZone)
+            {
+                textInputFieldHolder.SetActive(false);
+                questionText.text = "";
+                instruction = "";
+                instructionText.text = "";
+            }
+            if (questionAreaLogic.isInZone)
+            {
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+
+                    textInputFieldHolder.SetActive(true);
+                }
+                questionText.text = "2+2=?";
+                instruction = "Press 'E' to answer the question.";
+                instructionText.text = instruction;
             }
         }
     }
